1a53dee24500bef04138685f2da3f6348f4bbb5c
alex <13alexhancock13@gmail.com>
2025-07-08 19:45:22 +0000

    Tue, Jul 8, 2025, 7:45 PM +00:00

---
+++ game.py
@@ -0,0 +1,10 @@
+# File: main.py
+# File: game.py
+# this is a simple game where you dodge  falling asteroids
+# you can move left and right using the arrow keys
+import pygame # type: ignore
+import time
+import random
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("space dodge")
---
+++ geo.py
@@ -0,0 +1,13 @@
+# define SDL_MAIN_HANDLED
+# include <SDL.h>
+# include <SDL_image.h>
+# include <SDL_mixer.h>
+# include <SDL_ttf.h>
+# include <iostream>
+# include <vector>
+# include <sstream>
+
+int main()
+{
+    std::cout << "Hello, SDL!" << std::endl;
+    return 0;
