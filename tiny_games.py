a4f6b4e68bc6645d9089bbdc4f4ae099c8cd67c2
alex <13alexhancock13@gmail.com>
2025-07-08 21:24:42 +0000

    Tue, Jul 8, 2025, 9:24 PM +00:00

--- tiny_games.py
+++ tiny_games.py
@@ -4,3 +4,11 @@ import pygame
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+while TRUE:
+    # event loop
+    for event
+
+
+
+
