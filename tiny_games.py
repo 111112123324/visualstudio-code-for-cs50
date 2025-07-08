69c7c3b78dfe21bf644957421fd9680df678e655
alex <13alexhancock13@gmail.com>
2025-07-08 21:35:06 +0000

    Tue, Jul 8, 2025, 9:35 PM +00:00

--- tiny_games.py
+++ tiny_games.py
@@ -6,7 +6,7 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 runnning = True
 
-while running:
+while runnning:
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
